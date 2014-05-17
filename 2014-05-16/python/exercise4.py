507f30e3a98f568c3f6c1db1d5c5355b9ef8b31a
di-folca <and.difolca@stud.uniroma3.it>
2014-05-17 18:11:43 +0200

    aggiunto exercise4

--- 2014-05-16/python/exercise4.py
+++ 2014-05-16/python/exercise4.py
@@ -11,6 +11,27 @@ from mapper import *
 from boolean import *
 from sysml import *
 
+#rimuove le celle nell'array toRemove dal larmodel master
+def removeCells(master, toRemove):
+    V,CV = master
+    return V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+
+#definisce un lar model con le dimenzioni descritte dal size patterns
+def assemblyDiagram(sizePatterns):
+    return assemblyDiagramInit([len(f) for f in sizePatterns])(sizePatterns)
+
+#mostra gli indici della cella rispetto alla lista CV del larmodel
+def mostraNumeroCelle(larmodel, colore, grandezza):
+    hpc = SKEL_1(STRUCT(MKPOLS(larmodel)))
+    hpc = cellNumbering (larmodel,hpc)(range(len(larmodel[1])),colore,grandezza)
+    VIEW(hpc)
+
+def sommatore(lista):
+    res = .0
+    for a in lista:
+        res = a+res
+    return res
+
 
 """ 3D window to viewport transformation """
 def diagram2cellMatrix(diagram):
@@ -18,8 +39,8 @@ def diagram2cellMatrix(diagram):
       wdw = min(diagram[0]) + max(diagram[0])      # window3D [prende il piu piccolo punto e lo accorpa con il piu grande dei V, credo al solo fine di scorrerlo]
       cV = [master[0][v] for v in master[1][cell]] #suddivide i vertici per celle
       vpt = min(cV) + max(cV)                      # viewport3D
-      print "\n window3D =",wdw
-      print "\n viewport3D =",vpt
+      #print "\n window3D =",wdw
+      #print "\n viewport3D =",vpt
       
       mat = zeros((4,4))
       mat[0,0] = (vpt[3]-vpt[0])/(wdw[3]-wdw[0])
@@ -29,29 +50,58 @@ def diagram2cellMatrix(diagram):
       mat[2,2] = (vpt[5]-vpt[2])/(wdw[5]-wdw[2])
       mat[2,3] = vpt[2] - mat[2,2]*wdw[2]
       mat[3,3] = 1
-      print "\n mat =",mat
+      #print "\n mat =",mat
       return mat
    return diagramToCellMatrix0
 
+#filtra i V uguali tiene traccia di questo mappandoli
+def vertexSieve(master, diagram):
+   #dobbiamo separare 
+   return "ciao"
+
+
+help(boundaryOfChain)
+help(lar2boundaryFaces)
 
 def diagram2cell(diagram,master,cell):
    mat = diagram2cellMatrix(diagram)(master,cell)
-   diagram =larApply()(diagram)  
-   
-   """
+   diagram =larApply(mat)(diagram)  
+   #print diagram
+
    # yet to finish coding
    V, CV1, CV2, n12 = vertexSieve(master,diagram)
-   masterBoundaryFaces = boundaryOfChain(CV,FV)([cell])
-   diagramBoundaryFaces = lar2boundaryFaces(CV,FV)
-   """
-   V = master[0] + diagram[0]
-   offset = len(master[0])
-   CV = [c for k,c in enumerate(master[1]) if k != cell] + [
-         [v+offset for v in c] for c in diagram[1]]
-   master = V, CV
-   return master
 
+   #masterBoundaryFaces = boundaryOfChain(CV,FV)([cell])
+   #diagramBoundaryFaces = lar2boundaryFaces(CV,FV)
+
+   #V = master[0] + diagram[0]
+   #offset = len(master[0])
+   #CV = [c for k,c in enumerate(master[1]) if k != cell] + [#enumerate ritorna [[1, itemlista],[2,itemlista]] praticamente ricostruisce tutti i cv, eccetto quello della cell
+   #   [v+offset for v in c] for c in diagram[1]]#prende i vertici
+
+   master = V, CV1+CV2
+   return master
 
 
 print zeros((4,4))
 print [0,1] +[4,5]
+
+
+#####test
+ingressoPattern=[[0.3, (3.94-2.25-0.3), 0.3, 2.24, 0.3], [0.3, 2, 0.3, 3.10,0.3, 1.64], [0.3,3,0.3]]
+ingresso=assemblyDiagram(ingressoPattern)
+
+ingresso=removeCells(ingresso, range( (len(ingressoPattern[2])*len(ingressoPattern[1])*2) ) )#rimuove i primi 2 blocchi per ciascuna riga, altezza
+#mostraNumeroCelle(ingresso,RED,1)
+ingresso=removeCells(ingresso, [0,1,2,3,4,5,18,19,20,21,22,23,28,34])
+#DRAW(ingresso)
+
+#la porta dell'ingresso
+#mostraNumeroCelle(ingresso, CYAN, 2)
+porta= assemblyDiagram([[0.3],[1.5,1,(3.10-2.5)],[2,1]])
+ingresso = diagram2cell(porta,ingresso,32)
+ingresso=removeCells(ingresso,[41])
+#mostraNumeroCelle(ingresso,CYAN,2)
+porta2=assemblyDiagram([[0.3,1,(2.14-0.3-1)],[.3],[2,1]])
+ingresso = diagram2cell(porta2,ingresso,13)
+mostraNumeroCelle(ingresso,CYAN,2)
